83415d4d0309d601178f47b5a682b4e082bb3338
Sara <saradhashyam0401@gmail.com>
2023-09-06 23:31:06 +0530

    to check how cute you are

---
+++ button.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import tkinter.messagebox as msg
+
+root= Tk()
+root.geometry("900x600")
+
+Label(text="CUTE CUTE", bg="blue", font="sanserif 20 bold").pack(fill=X)
+Label(text="verify",bg="lightblue", font="sanserif 10 bold").pack(fill=X)
+Label().pack()
+Label(text="How much cute are YOU? ").pack()
+
+cvar = StringVar()
+# cvar.set("radio")
+
+def cfun():
+    msg.showinfo("CUTIE PIE", f" Maybe you are {cvar.get()}.... But YOU mean the WORLD for ME , my LOVE")
+
+cute = ["SUPER CUTE", "ULTRA CUTE", "LITTLE BIT CUTE", " CUTEST" ]
+
+for x in cute:
+    radio = Radiobutton(root, text=f"{x}", value=f"{x}", variable=cvar, bg="pink").pack(anchor="nw", fill=X)
+
+Button(root, text="LET'S SEE", command=cfun).pack()
+root.mainloop()
